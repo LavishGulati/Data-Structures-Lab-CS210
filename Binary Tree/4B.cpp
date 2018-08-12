361fc770c58b3aec8b15c7fb13e00450920e3726
LavishGulati <lavish00gulati@gmail.com>
2018-08-12 14:41:54 +0530

    Completed 4A and 4B in Binary Tree

--- Binary Tree/4B.cpp
+++ Binary Tree/4B.cpp
@@ -1,66 +1,199 @@
 #include <iostream>
 #include <fstream>
-#include <cstdlib>
 using namespace std;
 
-typedef struct node{
+typedef struct treeNode{
     int data;
-    struct node *left;
-    struct node *right;
-} node;
+    treeNode *left;
+    treeNode *right;
+} treeNode;
 
-node *createNode(int element){
-    node *newNode = new node;
-    newNode->data = element;
-    newNode->left = NULL;
-    newNode->right = NULL;
-    return newNode;
+typedef struct stackNode{
+    treeNode *data;
+    struct stackNode *next;
+} stackNode;
+
+typedef struct stack{
+    stackNode *head;
+    int size;
+} stack;
+
+stackNode *createStackNode(treeNode *element){
+    stackNode *newStackNode = new stackNode;
+    newStackNode->data = element;
+    newStackNode->next = NULL;
+    return newStackNode;
 }
 
-node *insertInBST(node *root, int element){
-    if(root == NULL){
-        root = createNode(element);
-        return root;
-    }
-
-    if(element < root->data){
-        node *newNode = insertInBST(root->left, element);
-        root->left = newNode;
-    }
-    else{
-        node *newNode = insertInBST(root->right, element);
-        root->right = newNode;
-    }
-
-    return root;
+stack *createStack(){
+    stack *s = new stack;
+    s->head == NULL;
+    s->size = 0;
+    return s;
 }
 
-void deleteTree(node *root){
-    if(root == NULL) return;
+treeNode *createTreeNode(int element){
+    treeNode *newTreeNode = new treeNode;
+    newTreeNode->data = element;
+    newTreeNode->left = NULL;
+    newTreeNode->right = NULL;
+    return newTreeNode;
+}
 
-    deleteTree(root->left);
-    deleteTree(root->right);
+treeNode *insertInBST(treeNode *treeRoot, int element){
+    if(treeRoot == NULL){
+        treeRoot = createTreeNode(element);
+        return treeRoot;
+    }
 
-    delete root;
+    if(element < treeRoot->data){
+        treeRoot->left = insertInBST(treeRoot->left, element);
+    }
+    else treeRoot->right = insertInBST(treeRoot->right, element);
+
+    return treeRoot;
+}
+
+void deleteTree(treeNode *treeRoot){
+    if(treeRoot == NULL) return;
+
+    deleteTree(treeRoot->left);
+    deleteTree(treeRoot->right);
+    delete treeRoot;
+}
+
+void push(stack *s, treeNode *element){
+    stackNode *newStackNode = createStackNode(element);
+    newStackNode->next = s->head;
+    s->head = newStackNode;
+    (s->size)++;
+}
+
+treeNode *pop(stack *s){
+    if(s->head == NULL) return NULL;
+
+    stackNode *answer = s->head;
+    s->head = answer->next;
+    (s->size)--;
+    return answer->data;
+}
+
+bool isEmpty(stack *s){
+    if(s->size == 0) return true;
+    else return false;
+}
+
+void deleteList(stackNode *head){
+    if(head == NULL) return;
+
+    deleteList(head->next);
+    delete head;
+}
+
+void deleteStack(stack *s){
+    deleteList(s->head);
+    delete s;
+}
+
+void inorder(treeNode *treeRoot){
+    stack *s = new stack;
+    treeNode *temp = treeRoot;
+    while(1){
+        while(temp != NULL){
+            push(s, temp);
+            temp = temp->left;
+        }
+
+        if(!isEmpty(s)){
+            temp = pop(s);
+            cout << temp->data << " ";
+            temp = temp->right;
+        }
+
+        if(isEmpty(s) && temp == NULL) break;
+    }
+
+    deleteStack(s);
+}
+
+void preorder(treeNode *treeRoot){
+    stack *s = createStack();
+    treeNode *temp = treeRoot;
+
+    while(1){
+        while(temp != NULL){
+            cout << temp->data << " ";
+            push(s, temp);
+            temp = temp->left;
+        }
+
+        if(!isEmpty(s)){
+            temp = pop(s);
+            // cout << temp->data << " ";
+            temp = temp->right;
+        }
+
+        if(isEmpty(s) && temp == NULL) break;
+    }
+
+    deleteStack(s);
+}
+
+treeNode *top(stack *s){
+    if(s->head == NULL) return NULL;
+    return s->head->data;
+}
+
+void postorder(treeNode *treeRoot){
+    stack *s = createStack();
+    treeNode *temp = treeRoot;
+
+    while(1){
+        while(temp != NULL){
+            if(temp->right != NULL) push(s, temp->right);
+            push(s, temp);
+            temp = temp->left;
+        }
+
+        if(!isEmpty(s)){
+            temp = pop(s);
+            treeNode *temp2 = top(s);
+            if(temp->right != NULL && temp2 == temp->right){
+                pop(s);
+                push(s, temp);
+                temp = temp2;
+            }
+            else{
+                cout << temp->data << " ";
+                temp = NULL;
+            }
+        }
+
+        if(isEmpty(s) && temp == NULL) break;
+    }
+
+    deleteStack(s);
 }
 
 int main(){
-    node *root = NULL;
-    srand((unsigned)time(0));
+    treeNode *treeRoot = NULL;
+    srand(unsigned(time(0)));
     for(int i = 0; i < 25; i++){
         int element = rand()%1000;
-        root = insertInBST(root, element);
+        treeRoot = insertInBST(treeRoot, element);
     }
 
-    cout << "Inorder: ";
-    inorder(root);
-    cout << endl << endl;
-    cout << "Preorder: ";
-    preorder(root);
-    cout << endl << endl;
-    cout << "Postorder: ";
-    postorder(root);
+    cout << "Inorder" << endl;
+    inorder(treeRoot);
     cout << endl << endl;
 
-    deleteTree(root);
+    cout << "Preorder" << endl;
+    preorder(treeRoot);
+    cout << endl << endl;
+
+    cout << "Postorder" << endl;
+    postorder(treeRoot);
+    cout << endl << endl;
+
+    deleteTree(treeRoot);
 }
